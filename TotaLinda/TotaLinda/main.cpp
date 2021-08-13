b795d8ca222e68240524c57e125cdee27032c417
TomasSchwartz <tschwartz@frba.utn.edu.ar>
2021-08-12 22:26:20 -0300

    Update main.cpp

--- TotaLinda/TotaLinda/main.cpp
+++ TotaLinda/TotaLinda/main.cpp
@@ -8,7 +8,17 @@ int main()
     int arrayNum [MAX]={3, 44, 65, 42, 79, 78, 33};
     bool arrayBool [MAX]={1, 0, 0, 1, 1, 0, 0};
     char finalArray[4*MAX]="(3&44|33)&(!79|78)";
+    int position[MAX];
 
+    short j=0;
+    for (short i=0;i<4*MAX;i++)
+    {
+        if (finalArray[i]==40 || finalArray[i]==41)
+        {
+            position[j]=i;
+            j++;
+        }
+    }
 
 
     return 0;
