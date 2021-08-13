308c7af785b9c17a97cf44fd48cbc32065102891
TomasSchwartz <84201896+TomasSchwartz@users.noreply.github.com>
2021-08-12 22:17:49 -0300

    Add files via upload

---
+++ TotaLinda/TotaLinda/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define MAX 7
+
+using namespace std;
+
+int main()
+{
+    int arrayNum [MAX]={3, 44, 65, 42, 79, 78, 33};
+    bool arrayBool [MAX]={1, 0, 0, 1, 1, 0, 0};
+    char finalArray[4*MAX]="(3&44|33)&(!79|78)";
+
+
+
+    return 0;
+}
